39bdce5243cf17e3c23d2a646f71c950e49aa9c0
Riizzy-IV <marce_lo_2011@hotmail.com>
2020-03-06 23:31:52 -0200

    Concluido os arquivos index.html, estilos.css, fotos.html e css2.css

---
+++ js/script01.js
@@ -0,0 +1,9 @@
+function abreFoto(x) {
+  if (x == 1)
+     document.getElementById("area").innerHTML = "<a href="index.html">home";
+   else if (x == 2)
+    document.getElementById("area").innerHTML = "<img src=img/car2.jpg>";
+   else if (x == 3)
+   document.getElementById("area").innerHTML = "<img src=img/car3.jpg>";
+   else if (x == 4)
+    document.getElementById("area").innerHTML = "<img src=img/moto4.jpg>";
